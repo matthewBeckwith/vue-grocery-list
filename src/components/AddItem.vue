27722c7c4128de2385fe4331d94454d89458ada7
Matthew Beckwith <kodahscripts@outlook.com>
2021-04-29 01:48:55 -0400

    started working on UI

---
+++ src/components/AddItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="form-content">
+    <form>
+      <div>
+        <input type="text" name="text" placeholder="Add Item" />
+      </div>
+      <div>
+        <input type="text" name="cost" placeholder="Cost per Unit" />
+      </div>
+      <div>
+        <input type="number" name="qty" placeholder="Quantity" />
+      </div>
+      <div class="check">
+        <label>Coupon?</label>
+        <input type="checkbox" name="coupon" />
+      </div>
+
+      <input type="submit" value="ADD" class="btn" />
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.form-content {
+  display: flex;
+  max-width: 400px;
+}
+input {
+  padding: 10px 20px;
+  margin-top: 10px;
+}
+.check {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: baseline;
+}
+.btn {
+  display: block;
+  width: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,31 @@
 <template>
-  <p>hello</p>
+  <div class="main-content">
+    <AddItem />
+    <h3>List of items</h3>
+  </div>
 </template>
 
 <script>
+import AddItem from "../components/AddItem";
+
 export default {
   name: "Home",
+  components: {
+    AddItem,
+  },
 };
 </script>
+
+<style scoped>
+.main-content {
+  display: flex;
+  justify-content: space-between;
+  max-width: 95%;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+</style>
